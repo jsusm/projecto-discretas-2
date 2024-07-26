559f79f5e96d998978e26978cf93c162176ffbc0
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-07-26 15:36:20 -0400

    Aniadir funcion de suma

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int sum(int a, int b) {
+  return a + b;
+}
+
 int main(){
-  std::cout << "Hello world" << std::endl;
+  std::cout << "Hello world"<< sum(1, 2) << std::endl;
 }
