0c7306aadbe1c93882b1e5e13d0d518dfb8c2ee9
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-07-31 15:29:00 -0400

    entrada de los primeros datos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
-int sum(int a, int b) {
-  return a + b;
+using namespace std;
+
+void entrada() {
+  int n_bases, n_pipes;
+  cin >> n_bases >> n_pipes;
+  cout << "n_bases: " << n_bases<< " n_pipes: " << n_pipes<< endl;
+
+  int startinNode, targetNode;
+  cin >> startinNode >> targetNode;
+  cout<< "startinNode: " << startinNode<< " targetNode: " << targetNode << endl;
+
+  int totalTime;
+  cin >> totalTime;
+  cout << "totalTime: " << totalTime << endl;
 }
 
 int main(){
-  std::cout << "Hello world"<< sum(1, 2) << std::endl;
+  entrada();
 }
