90a72558b65dda518ce7c0369780746db74aa014
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-07-31 17:10:12 -0400

    implement dijkstra algorithm

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 #include <list>
 
@@ -7,11 +8,22 @@ class Vertex {
 public:
   bool explored;
   int id;
+  int minTime;
   list<Vertex *> path;
 
   Vertex(int id) {
     explored = false;
     this->id = id;
+    minTime = INT_MAX;
+  }
+
+  void print() {
+    cout << (explored ? "T" : "F") << " " << id << " {time: " << minTime
+         << ", path: [";
+    for (Vertex *v : path) {
+      cout << v->id << ", ";
+    }
+    cout << "]}" << endl;
   }
 };
 
@@ -36,8 +48,59 @@ list<Edge *> getEdges(Vertex *v, list<Edge *> edges) {
   return out;
 }
 
-int main() {
+void dijktra(list<Edge *> edges, list<Vertex *> vertices, int startingNodeId,
+             int targetNodeId) {
+  Vertex *starting;
+  Vertex *target;
+  // buscamos el nodo de inicio y del final
+  for (Vertex *v : vertices) {
+    if (v->id == startingNodeId) {
+      starting = v;
+    }
+    if (v->id == targetNodeId) {
+      target = v;
+    }
+  }
 
+  starting->minTime = 0;
+  starting->explored = true;
+
+  Vertex *currVertex = starting;
+  while (currVertex->id != targetNodeId) {
+    list<Edge *> vertexEdges = getEdges(currVertex, edges);
+    // Actualizamos los tiempos minimos
+    for (Edge *e : vertexEdges) {
+      Vertex *connectionVertex = e->A->id == currVertex->id ? e->B : e->A;
+
+      if (currVertex->minTime + e->weight < connectionVertex->minTime) {
+        connectionVertex->minTime = currVertex->minTime + e->weight;
+        connectionVertex->path = currVertex->path;
+        connectionVertex->path.push_back(currVertex);
+      }
+    }
+    // Buscamos el vertice que tenga menor tiempor y lo exploramos
+    Vertex *minVertex = nullptr;
+    for (Vertex *v : vertices) {
+      v->print();
+      if (v->explored)
+        continue;
+      if (minVertex == nullptr) {
+        minVertex = v;
+      }
+      if (v->minTime < minVertex->minTime) {
+        minVertex = v;
+      }
+    }
+    minVertex->explored = true;
+    currVertex = minVertex;
+    currVertex->print();
+  }
+
+  currVertex->print();
+}
+
+int main() {
+  /// Entrada
   list<Vertex *> vertices;
   list<Edge *> edges;
 
@@ -46,7 +109,7 @@ int main() {
   cout << "n_bases: " << n_bases << " n_pipes: " << n_pipes << endl;
 
   int startinNode, targetNode;
-  cin >> startinNode >> targetNode;
+  cin >> targetNode >> startinNode;
   cout << "startinNode: " << startinNode << " targetNode: " << targetNode
        << endl;
 
@@ -61,6 +124,7 @@ int main() {
     Vertex *nodeA = nullptr;
     Vertex *nodeB = nullptr;
     for (Vertex *v : vertices) {
+      // asignamos si exsisten
       if (v->id == a) {
         nodeA = v;
       }
@@ -68,6 +132,7 @@ int main() {
         nodeB = v;
       }
     }
+    // si no existen los creamos
     if (nodeA == nullptr) {
       nodeA = new Vertex(a);
       vertices.push_back(nodeA);
@@ -76,14 +141,13 @@ int main() {
       nodeB = new Vertex(b);
       vertices.push_back(nodeB);
     }
-
+    // aniadimos la arista
     Edge *edge = new Edge(weight);
     edge->A = nodeA;
     edge->B = nodeB;
     edges.push_front(edge);
   }
   // print all nodes
-
   for (Vertex *v : vertices) {
     cout << v->id << ": ";
     list<Edge *> vertexEdges = getEdges(v, edges);
@@ -93,4 +157,5 @@ int main() {
     }
     cout << endl;
   }
+  dijktra(edges, vertices, startinNode, targetNode);
 }
